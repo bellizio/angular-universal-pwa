e48354fccca1cb7d5558e73b75ea5d91a9993b6c
Devon Bellizio <jdbellizio@gmail.com>
2017-11-10 12:54:16 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    // BrowserModule
+    BrowserModule.withServerTransition({ appId: 'ng-universal-pwa' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string
+  ) {
+    const platform = isPlatformBrowser(platformId) ? 'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
